3f98feca9e17f41d47dd450f020c5b37ee5ab900
Matheus Módolo <nmmatheus@hotmail.com>
2023-03-15 20:55:20 -0300

    melhorias no menu

--- Projeto5 - Infusion/js/main.js
+++ Projeto5 - Infusion/js/main.js
@@ -1,3 +1,10 @@
+// comparar os arquivos:
+// main.js
+// menu.js
+// index.html
+// main.css
+// acho q eu não importei alguma coisa no index.html
+
 (function () {
     var $body = document.querySelector("body");
     $body.classList.remove("no-js");
@@ -6,7 +13,7 @@
     var menu = new Menu({
         container: ".header__nav",
         toggleBtn: ".header__btnMenu",
-        widthEnabled: 1024
+        widthEnabled: 1025
     })
 
 })
