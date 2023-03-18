78082f7d255b7575115dc92a0a1d362bad383cce
Matheus Módolo <nmmatheus@hotmail.com>
2023-03-18 19:01:44 -0300

    finalizado

--- Projeto5 - Infusion/js/carousel.js
+++ Projeto5 - Infusion/js/carousel.js
@@ -1,42 +1,43 @@
 function Carousel(config) {
+    this.container = (typeof config.container === 'string') ? document.querySelector(config.container) : config.container
 
-    this.container = (typeof config.container === "string") ? document.querySelector(config.container) : config.container;
-    this.itens = (typeof config.itens === "string") ? this.container.querySelectorAll(config.itens) : config.itens;
-    this.btnPrev = (typeof config.btnPrev === "string") ? this.container.querySelector(config.btnPrev) : config.btnPrev;
-    this.btnNext = (typeof config.itenbtnNexts === "string") ? this.container.querySelector(config.btnNext) : config.btnNext;
+    this.itens = (typeof config.itens === 'string') ? this.container.querySelectorAll(config.itens) : config.itens
+
+    this.btnPrev = (typeof config.btnPrev === 'string') ? this.container.querySelector(config.btnPrev) : config.btnPrev
+
+    this.btnNext = (typeof config.btnNext === 'string') ? this.container.querySelector(config.btnNext) : config.btnNext
 
     var _this = this;
-    var _currentSlide = 0;
+    var _currentSlide = 0
 
-    init();
+    init()
 
     function init() {
-        var _show = _this.container.querySelectorAll(".show");
+        var _show = _this.container.querySelectorAll('.show')
 
         Array.prototype.forEach.call(_show, function (sh) {
-            sh.classList.remove("show");
-        });
+            sh.classList.remove('show')
+        })
+        _this.itens[0].classList.add('show')
+        _this.btnNext.removeAttribute('style')
+        _this.btnPrev.removeAttribute('style')
 
-        _this.itens[0].classList.add("show");
-        _this.btnPrev.removeAttribute("style");
-        _this.btnNext.removeAttribute("style");
-
-        addListeners();
+        addListeners()
     }
 
     function addListeners() {
-        _this.btnNext.addEventListener("click", showNextSlide);
-        _this.btnPrev.addEventListener("click", showPrevSlide);
+        _this.btnNext.addEventListener('click', showNextSlide)
+        _this.btnPrev.addEventListener('click', showPrevSlide)
     }
 
     function showNextSlide() {
         _currentSlide++;
-        showSlide();
+        showSlide()
     }
 
     function showPrevSlide() {
         _currentSlide--;
-        showSlide();
+        showSlide()
     }
 
     function showSlide() {
@@ -44,7 +45,8 @@ function Carousel(config) {
         var slide = _currentSlide % qtd;
         slide = Math.abs(slide);
 
-        _this.container.querySelector(".show").classList.remove("show");
-        _this.itens[slide].classList.add("show");
+        _this.container.querySelector('.show').classList.remove('show');
+        _this.itens[slide].classList.add('show')
+
     }
 }
--- Projeto5 - Infusion/js/main.js
+++ Projeto5 - Infusion/js/main.js
@@ -1,26 +1,25 @@
 (function () {
-    var $body = document.querySelector("body");
-    $body.classList.remove("no-js")
-    $body.classList.add("js")
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
 
     var menu = new Menu({
-        container: ".header__nav",
-        toggleBtn: ".header__btnMenu",
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
         widthEnabled: 1024
     })
 
     var carouselImgs = new Carousel({
-        container: ".laptop-slider .slideshow",
-        itens: "figure",
-        btnPrev: ".prev",
-        btnNext: ".next"
-    });
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
 
     var carouselQuotes = new Carousel({
-        container: ".quote-slideshow",
-        itens: "figure",
-        btnPrev: ".prev",
-        btnNext: ".next"
-    });
-
-})
+        container: '.quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+})()
